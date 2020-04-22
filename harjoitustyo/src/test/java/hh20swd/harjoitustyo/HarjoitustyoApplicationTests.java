da544cddb6defb914510b871c3021b51b1b5bb79
elinapiispanen <elinapiispanen1@hotmail.fi>
2020-04-22 21:59:19 +0300

    testausta

--- harjoitustyo/src/test/java/hh20swd/harjoitustyo/HarjoitustyoApplicationTests.java
+++ harjoitustyo/src/test/java/hh20swd/harjoitustyo/HarjoitustyoApplicationTests.java
@@ -1,13 +1,26 @@
 package hh20swd.harjoitustyo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import hh20swd.harjoitustyo.webcontroller.SarjaController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class HarjoitustyoApplicationTests {
 
+	@Autowired
+	private SarjaController controller;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
